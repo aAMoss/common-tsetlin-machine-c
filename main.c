da8a4953701da3faa0ad378db8e798bfe171c937
aAMoss <92592883+aAMoss@users.noreply.github.com>
2022-09-28 15:53:29 +0100

    Got test and training data, defined some variables and placeholder functions.

---
+++ main.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h> 
+#include <string.h> 
+
+
+#define CLAUSES 64
+#define TA 30
+
+#define S_FEEDBACK 3.9
+#define THRESHOLD 15
+
+
+#define TEST_SAMPLES 5000
+#define TRAIN_SAMPLES 5000
+#define INPUT_SIZE 13
+#define CLASS_SIZE 1
+
+
+
+#define EPOCHS 100
+#define TRAIN_EXAMPLES 1000
+#define TEST_EXAMPLES 2000
+
+
+int get_input_bools()
+{
+
+    
+
+}
+
+
+int make_literal_bools()
+{
+
+
+
+}
+
+
+
+
+int init_tsetlin_machine()
+{
+
+
+
+
+}
+
+int compute_clause()
+{
+
+
+
+
+
+}
+
+int feedback_type2()
+{
+
+
+}
+
+    int feedback_type2()
+{
+
+    
+}
+
+
+
+
+int main(void)
+{
+
+
+
+    
+    
+    return 0;
+
+}
