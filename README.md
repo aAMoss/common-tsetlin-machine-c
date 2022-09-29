c98840736d1929785665673dddc152f4fca85149
aAMoss <92592883+aAMoss@users.noreply.github.com>
2022-09-29 11:53:59 +0100

    function to create boolean literals added

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # common-tsetlin-machine-c
 
+
+## Overview
 Implementation of the basic Tsetlin Machine in C from the paper: [The Tsetlin Machine - A Game Theoretic Bandit Driven Approach to Optimal Pattern Recognition with Propositional Logic](https://arxiv.org/abs/1804.01508).
+
+
+## TO DO LIST
+
+- [ ] Makefile
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <stdlib.h> 
 #include <string.h> 
+#include <time.h>
 
 
 #define SUCCESS 0
@@ -19,20 +20,29 @@
 
 #define DATA_SAMPLES 5000
 #define FEATURES 13 //includes the class number
-
+#define LITERALS (FEATURES * 2)
 
 
 #define EPOCHS 100
 
 
 
-FILE *Train_Sample_File;
-FILE *Test_Sample_File;
+//FILE *Train_Sample_File;
+FILE *Input_Sample_File;
  
 int TrainDataBools[DATA_SAMPLES][FEATURES];
 int TestDataBools[DATA_SAMPLES][FEATURES];
 
-int get_input_bools(FILE *fp, int data[DATA_SAMPLES][FEATURES], char *fname)
+int TrainingLiterals[DATA_SAMPLES][LITERALS];
+int TestingLiterals[DATA_SAMPLES][LITERALS];
+
+int read_input_file(FILE *fp, int data[DATA_SAMPLES][FEATURES], char *fname);
+int make_bool_literals(int input[DATA_SAMPLES][FEATURES], int output[DATA_SAMPLES][LITERALS]);
+void print_input(int data[DATA_SAMPLES][FEATURES]);
+void print_literals(int data[DATA_SAMPLES][LITERALS]);
+
+
+int read_input_file(FILE *fp, int data[DATA_SAMPLES][FEATURES], char *fname)
 {
     
     char buffer[256];
@@ -69,13 +79,17 @@ int get_input_bools(FILE *fp, int data[DATA_SAMPLES][FEATURES], char *fname)
     fclose(fp);
 
     return SUCCESS;
+    
 }
 
-void print_data(int data[DATA_SAMPLES][FEATURES])
+void print_input(int data[DATA_SAMPLES][FEATURES])
 {
-
+    
     for(int i = 0; i < DATA_SAMPLES; i++)
     {
+
+        printf("Sample %d\t",i);
+        
         for(int j = 0; j < FEATURES; j++)
         {
             printf("%d", data[i][j]);
@@ -84,38 +98,86 @@ void print_data(int data[DATA_SAMPLES][FEATURES])
         printf("\n");
     }
 
-
 }
 
 
-int main(void)
+
+int make_bool_literals(int input[DATA_SAMPLES][FEATURES], int output[DATA_SAMPLES][LITERALS])
 {
 
-    if( get_input_bools(Train_Sample_File, TrainDataBools, TRAIN_DATA) != 1 )
+    for(int i = 0; i < DATA_SAMPLES; i++)
     {
-        puts("Training data, acquired!");
-    }
-    if( get_input_bools(Test_Sample_File, TestDataBools, TEST_DATA) != 1 )
-    {
-         puts("Test data, acquired!");
+       
+        for(int j = 0; j < FEATURES; j++)
+        {   
+     
+            switch(input[i][j])
+            {
+                case 0: output[i][j] = 0; output[i][j+FEATURES] = 1; break;
+                case 1: output[i][j] = 1; output[i][j+FEATURES] = 0; break;
+                default: break;
+            }
+   
+            //printf("Sample %d Feature %d Input %d Comp %d\n", i, j, input[i][j], output[i][j+FEATURES]);
+            
+        }
+
     }
 
-    //print_data(TrainDataBools);
-    //print_data(TestDataBools);
+    return SUCCESS;
+}
+
+void print_literals(int data[DATA_SAMPLES][LITERALS])
+{
+    
+    for(int i = 0; i < DATA_SAMPLES; i++)
+    {
+        printf("Sample %d\t",i);
+
+        for(int j = 0; j < FEATURES; j++)
+        {
+            printf("%d", data[i][j]);
+        }
+
+        printf(" ");
+
+        for(int j = FEATURES; j < LITERALS; j++)
+        {
+            printf("%d", data[i][j]);
+        }
+
+        printf("\n");
+    }
+
+}
+
+int main(void)
+{
+    srand(time(NULL));
+
+    read_input_file(Input_Sample_File, TrainDataBools, TRAIN_DATA);
+    read_input_file(Input_Sample_File, TestDataBools, TEST_DATA);
+    puts("Training and Testing Data aquired!");
+    
+    make_bool_literals(TrainDataBools, TrainingLiterals);
+    make_bool_literals(TestDataBools, TestingLiterals);
+    puts("Training and Testing Literals acquired!");
+    
 
     
+   
+    //print_input(TrainDataBools);
+    //print_input(TestDataBools);
+    //print_literals(TrainingLiterals);
+    //print_literals(TestingLiterals);
+
     return 0;
 
 }
 
 
 
-int make_literal_bools()
-{
 
-
-
-}
 
 
 
